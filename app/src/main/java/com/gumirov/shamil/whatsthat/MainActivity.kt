9c0f2bd2bd2b6864a766965a18049ddddcd5b59d
Shamil Gumirov <shamil@gumirov.com>
2019-12-07 19:23:02 +0100

    Initial CameraX implementation for image analyzing

---
+++ app/src/main/java/com/gumirov/shamil/whatsthat/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.gumirov.shamil.whatsthat
+
+import android.os.Bundle
+import android.view.View
+import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity
+  : AppCompatActivity()
+{
+  private lateinit var container: FrameLayout
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    container = findViewById(R.id.fragment_container)
+  }
+
+  override fun onResume() {
+    super.onResume()
+    // Before setting full screen flags, we must wait a bit to let UI settle; otherwise, we may
+    // be trying to set app to immersive mode before it's ready and the flags do not stick
+    container.postDelayed({
+      container.systemUiVisibility = FLAGS_FULLSCREEN
+    }, IMMERSIVE_FLAG_TIMEOUT)
+  }
+
+  companion object {
+    private const val IMMERSIVE_FLAG_TIMEOUT = 500L
+    const val FLAGS_FULLSCREEN =
+        View.SYSTEM_UI_FLAG_LOW_PROFILE or
+            View.SYSTEM_UI_FLAG_FULLSCREEN or
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+  }
+}
---
+++ app/src/main/java/com/gumirov/shamil/whatsthat/PermissionRequestFragment.kt
@@ -0,0 +1,52 @@
+package com.gumirov.shamil.whatsthat
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+
+class PermissionRequestFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (!hasPermissions(requireContext())) {
+            // Request camera-related permissions
+            requestPermissions(PERMISSIONS_REQUIRED, PERMISSIONS_REQUEST_CODE)
+        } else {
+            // If permissions have already been granted, proceed
+            Navigation.findNavController(requireActivity(), R.id.fragment_container).navigate(
+                PermissionRequestFragmentDirections.actionPermissionsToCamera())
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSIONS_REQUEST_CODE) {
+            if (PackageManager.PERMISSION_GRANTED == grantResults.firstOrNull()) {
+                // Take the user to the success fragment when permission is granted
+                Toast.makeText(context, "Permission request granted", Toast.LENGTH_LONG).show()
+                Navigation.findNavController(requireActivity(), R.id.fragment_container).navigate(
+                    PermissionRequestFragmentDirections.actionPermissionsToCamera())
+            } else {
+                Toast.makeText(context, "Permission request denied", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    companion object {
+        private const val PERMISSIONS_REQUEST_CODE = 10
+        private val PERMISSIONS_REQUIRED = arrayOf(Manifest.permission.CAMERA)
+
+
+        /** Convenience method used to check if all permissions required by this app are granted */
+        fun hasPermissions(context: Context) = PERMISSIONS_REQUIRED.all {
+            ContextCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+}
