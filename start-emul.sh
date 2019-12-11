6a077c095adac4d636a6f7b4a91e2bbbe52c5fee
Shamil Gumirov <shamil@gumirov.com>
2019-12-11 20:37:42 +0100

    IC-11: Restart adb to enumerate devices before tests

--- start-emul.sh
+++ start-emul.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-adb kill-server
+$ANDROID_HOME/platform-tools/adb kill-server
 
 name='avd'
 [[ $(docker ps -f "name=$name" --format '{{.Names}}') == $name ]] || docker run -d --rm -p 5555:5555 -p 5554:5554 --name $name --device /dev/kvm swind/android-emulator
