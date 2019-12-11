a486bc61c724d3548be6a22d84a77b28f7c362d3
Shamil Gumirov <shamil@gumirov.com>
2019-12-11 20:12:34 +0100

    IC-8: Clean the result text format

--- app/src/main/java/com/gumirov/shamil/whatsthat/AnalysisResultViewModel.kt
+++ app/src/main/java/com/gumirov/shamil/whatsthat/AnalysisResultViewModel.kt
@@ -1,6 +1,7 @@
 package com.gumirov.shamil.whatsthat
 
 import android.graphics.Bitmap
+import android.graphics.Color
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
@@ -12,5 +13,6 @@ class AnalysisResultViewModel @Inject constructor(private val classifier: ImageC
 {
   val image = MutableLiveData<Bitmap>()
   private val resultList = Transformations.map(image) { classifier.recognizeImage(it) }
-  val result = Transformations.map(resultList) { if (it?.size ?: 0 > 0) it[0].toString() else "Definitely not a hotdog" }
+  val result = Transformations.map(resultList) {
+    if (it?.size ?: 0 > 0) String.format("%s (%.1f%%)", it[0].title, it[0].confidence * 100f) else "Definitely not a hotdog" }
 }
